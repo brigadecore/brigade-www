e622409d7fc9c1e9c192eccc6f4eb84200d31b73
flynnduism <flynnduism@gmail.com>
2017-12-02 22:47:08 -0800

    pipe background layout

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,16 @@
-$(document).foundation();
+$(document).foundation({
+  orbit: {
+    stack_on_small: false,
+    navigation_arrows: false,
+    slide_number: false,
+    pause_on_hover: true,
+    resume_on_mouseout: false,
+    bullets: true,
+    timer: true,
+    variable_height: false,
+  }
+});
 
 $( document ).ready(function() {
-  console.log( "jQuery!" );
+  // console.log( "jQuery!" );
 });
